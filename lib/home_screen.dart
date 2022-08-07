029dbce7b17e4134bd7caed4f7a61071b5b2112b
morawer <morala84@gmail.com>
2022-08-07 17:21:20 +0200

    new project ADDED

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,10 +11,66 @@ class HomeScreen extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          centerTitle: true,
+          title: const Text('Calculadora'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                alignment: const Alignment(0.8, 0.8),
+                height: 250.0,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 3.0, color: Colors.black12),
+                    borderRadius: BorderRadius.circular(15.0)),
+                child: const Text('Prueba 123',
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              ),
+              Center(
+                child: Expanded(
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Expanded(
+                          child: OutlinedButton(
+                              onPressed: () {},
+                              child: const Text('1',
+                                  style: TextStyle(fontSize: 42))),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Expanded(
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                child: const Text('2',
+                                    style: TextStyle(fontSize: 42)))),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Expanded(
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                child: const Text('3',
+                                    style: TextStyle(fontSize: 42)))),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Expanded(
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                child: const Text('+',
+                                    style: TextStyle(fontSize: 42)))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
