e9ec93e1f48af99a7e123fab8d9545a622833ab8
morawer <morala84@gmail.com>
2022-08-08 20:27:30 +0200

    buttons ADDED

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const HomeScreen());
+String output = '0';
+pressedButton(String buttonText) {
+  output = buttonText;
+}
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  final int number = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +26,24 @@ class HomeScreen extends StatelessWidget {
                 height: 250.0,
                 decoration: BoxDecoration(
                     border: Border.all(width: 3.0, color: Colors.black12),
+                    color: Colors.grey[100],
                     borderRadius: BorderRadius.circular(15.0)),
-                child: const Text('Prueba 123',
-                    style:
-                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                child: Text(output,
+                    style: const TextStyle(
+                        fontSize: 56, fontWeight: FontWeight.bold)),
               ),
-              const SizedBox(height: 50),
+              const SizedBox(height: 30),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _creatorButton('C'),
+                  const Expanded(child: SizedBox(height: 50)),
+                  const Expanded(child: SizedBox(height: 50)),
+                  _creatorButton('/'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _creatorButton('1'),
                   _creatorButton('2'),
@@ -39,6 +52,7 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _creatorButton('4'),
                   _creatorButton('5'),
@@ -47,11 +61,21 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _creatorButton('7'),
                   _creatorButton('8'),
                   _creatorButton('9'),
-                  _creatorButton('X'),
+                  _creatorButton('x'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(child: SizedBox(height: 80)),
+                  Expanded(
+                      child: SizedBox(height: 80, child: _creatorButton('0'))),
+                  SizedBox(height: 80, width: 185, child: _creatorButton('=')),
                 ],
               ),
             ],
@@ -63,18 +87,23 @@ class HomeScreen extends StatelessWidget {
 }
 
 Widget _creatorButton(String buttonText) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Expanded(
-        child: ElevatedButton(
-      style: ButtonStyle(
-          elevation: MaterialStateProperty.all(10),
-          backgroundColor: MaterialStateProperty.all(Colors.green)),
-      onPressed: () {},
-      child: Text(
-        buttonText,
-        style: const TextStyle(fontSize: 52, color: Colors.black),
+  return Expanded(
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            elevation: MaterialStateProperty.all(10),
+            backgroundColor: MaterialStateProperty.all(Colors.grey[100])),
+        onPressed: () {
+          pressedButton(buttonText);
+          // ignore: avoid_print
+          print(buttonText);
+        },
+        child: Text(
+          buttonText,
+          style: const TextStyle(fontSize: 52, color: Colors.black),
+        ),
       ),
-    )),
+    ),
   );
 }
