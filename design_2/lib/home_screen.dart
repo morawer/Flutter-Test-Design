37ecd6279c90e9178dbc5f7da61b5d951189e9e8
morawer <morala84@gmail.com>
2022-08-13 17:53:28 +0200

    ADDED gitignore

--- design_2/lib/home_screen.dart
+++ design_2/lib/home_screen.dart
@@ -80,21 +80,14 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _creatorButton(String buttonText) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-            elevation: MaterialStateProperty.all(10),
-            backgroundColor: MaterialStateProperty.all(Colors.grey[100])),
+    return TextButton(
         onPressed: () {
           pressedButton(buttonText);
         },
         child: Text(
           buttonText,
-          style: const TextStyle(fontSize: 52, color: Colors.black),
-        ),
-      ),
-    );
+          style: const TextStyle(fontSize: 42.0),
+        ));
   }
 
   @override
@@ -106,69 +99,66 @@ class _HomeScreenState extends State<HomeScreen> {
           centerTitle: true,
           title: const Text('Calculadora'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  alignment: const Alignment(0.8, 0.8),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 3.0, color: Colors.black12),
-                      color: Colors.grey[100],
-                      borderRadius: BorderRadius.circular(15.0)),
-                  child: Text(output,
-                      style: const TextStyle(
-                          fontSize: 56, fontWeight: FontWeight.bold)),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: const Alignment(0.8, 0.8),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 3.0, color: Colors.black12),
+                  color: Colors.grey[100],
                 ),
+                child: Text(output,
+                    style: const TextStyle(
+                        fontSize: 56, fontWeight: FontWeight.bold)),
               ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('C'),
-                  const SizedBox(height: 50, width: 83),
-                  const SizedBox(height: 50, width: 83),
-                  _creatorButton('/'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('7'),
-                  _creatorButton('8'),
-                  _creatorButton('9'),
-                  _creatorButton('x'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('4'),
-                  _creatorButton('5'),
-                  _creatorButton('6'),
-                  _creatorButton('+'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('1'),
-                  _creatorButton('2'),
-                  _creatorButton('3'),
-                  _creatorButton('-'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 80, child: _creatorButton('.')),
-                  SizedBox(height: 80, child: _creatorButton('0')),
-                  SizedBox(height: 80, width: 166, child: _creatorButton('=')),
-                ],
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _creatorButton('C'),
+                const SizedBox(height: 50, width: 83),
+                const SizedBox(height: 50, width: 83),
+                _creatorButton('/'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _creatorButton('7'),
+                _creatorButton('8'),
+                _creatorButton('9'),
+                _creatorButton('x'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _creatorButton('4'),
+                _creatorButton('5'),
+                _creatorButton('6'),
+                _creatorButton('+'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _creatorButton('1'),
+                _creatorButton('2'),
+                _creatorButton('3'),
+                _creatorButton('-'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 80, child: _creatorButton('.')),
+                SizedBox(height: 80, child: _creatorButton('0')),
+                SizedBox(height: 80, width: 166, child: _creatorButton('=')),
+              ],
+            ),
+          ],
         ),
       ),
     );
