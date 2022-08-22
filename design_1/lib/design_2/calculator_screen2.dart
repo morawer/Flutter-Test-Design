e627a3769a1dceee4a14ed20a618bb2a87195c5b
morawer <morala84@gmail.com>
2022-08-22 20:29:04 +0200

    in process

--- design_1/lib/design_2/calculator_screen2.dart
+++ design_1/lib/design_2/calculator_screen2.dart
@@ -79,99 +79,90 @@ class _CalculatorScreen2State extends State<CalculatorScreen2> {
     });
   }
 
-  Widget _creatorButton(String buttonText) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-            elevation: MaterialStateProperty.all(10),
-            backgroundColor: MaterialStateProperty.all(Colors.grey[100])),
-        onPressed: () {
-          pressedButton(buttonText);
-        },
-        child: Text(
-          buttonText,
-          style: const TextStyle(fontSize: 52, color: Colors.black),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Calculadora'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  alignment: const Alignment(0.8, 0.8),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 3.0, color: Colors.black12),
-                      color: Colors.grey[100],
-                      borderRadius: BorderRadius.circular(15.0)),
-                  child: Text(output,
-                      style: const TextStyle(
-                          fontSize: 56, fontWeight: FontWeight.bold)),
-                ),
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('C'),
-                  const SizedBox(height: 50, width: 83),
-                  const SizedBox(height: 50, width: 83),
-                  _creatorButton('/'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('7'),
-                  _creatorButton('8'),
-                  _creatorButton('9'),
-                  _creatorButton('x'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('4'),
-                  _creatorButton('5'),
-                  _creatorButton('6'),
-                  _creatorButton('+'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _creatorButton('1'),
-                  _creatorButton('2'),
-                  _creatorButton('3'),
-                  _creatorButton('-'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 80, child: _creatorButton('.')),
-                  SizedBox(height: 80, child: _creatorButton('0')),
-                  SizedBox(height: 80, width: 166, child: _creatorButton('=')),
-                ],
-              ),
-            ],
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Calculadora'),
           ),
-        ),
-      ),
+          body: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    alignment: Alignment.centerRight,
+                    margin: const EdgeInsets.only(bottom: 20),
+                    width: double.infinity,
+                    child:
+                        const Text('RESULT', style: TextStyle(fontSize: 36))),
+                const Divider(thickness: 3.0, color: Colors.white60),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              primary: Colors.white,
+                              shape: const StadiumBorder()),
+                          child: const SizedBox(
+                              height: 65,
+                              width: 65,
+                              child: Center(
+                                  child: Text('1',
+                                      style: TextStyle(
+                                          fontSize: 36,
+                                          fontWeight: FontWeight.bold)))),
+                          onPressed: () {}),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              primary: Colors.white,
+                              shape: const StadiumBorder()),
+                          child: const SizedBox(
+                              height: 65,
+                              width: 65,
+                              child: Center(
+                                  child: Text('2',
+                                      style: TextStyle(
+                                          fontSize: 36,
+                                          fontWeight: FontWeight.bold)))),
+                          onPressed: () {}),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: TextButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              primary: Colors.white,
+                              shape: const StadiumBorder()),
+                          child: const SizedBox(
+                              height: 65,
+                              width: 65,
+                              child: Center(
+                                  child: Text('3',
+                                      style: TextStyle(
+                                          fontSize: 36,
+                                          fontWeight: FontWeight.bold)))),
+                          onPressed: () {}),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
